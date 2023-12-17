748ead4bd6a4490d1360ea9d07f457188cd3458f
Nikita15423451 <154085437+Nikita15423451@users.noreply.github.com>
2023-12-17 22:57:39 +0300

    Update kot.py

--- kot.py
+++ kot.py
@@ -36,28 +36,15 @@ for line in conversations:
 
 input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
 
-# Проверка данных перед обучением
-if input_sequences.size == 0:
-    print("Данные для обучения пустые. Пожалуйста, добавьте данные для обучения.")
-    exit()
-
 # Формирование xs и ys из input_sequences
 xs = input_sequences[:, :-1]
 ys = input_sequences[:, -1]
 ys = tf.keras.utils.to_categorical(ys, num_classes=total_words)
 
-# Проверка размерности данных перед обучением
-if xs.size == 0 or ys.size == 0:
-    print("Формат данных для обучения некорректен. Пожалуйста, проверьте данные перед обучением.")
-    exit()
-
-# Определение модели
 model = tf.keras.Sequential([
     tf.keras.layers.Embedding(total_words, 100, input_length=max_sequence_len-1),
     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(150, return_sequences=True)),
     tf.keras.layers.Dropout(0.2),
-    # Добавьте больше слоев LSTM, Dropout и другие слои по вашему выбору
-    # Пример:
     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(150, return_sequences=True)),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(150)),
@@ -65,12 +52,10 @@ model = tf.keras.Sequential([
 ])
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print(xs.shape, ys.shape)  # Проверка размера данных перед обучением
+
 model.fit(xs, ys, epochs=5000, verbose=1, batch_size=64)
-
-# Остальной код для бота
-# ...
-
-
 
 async def generate_response(seed_text):
     # Ваш код для генерации ответа на основе обученной модели
