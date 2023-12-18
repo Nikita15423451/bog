ee4ecd8dc4a6fada5e93996579e167d365039d7a
Nikita15423451 <154085437+Nikita15423451@users.noreply.github.com>
2023-12-18 19:58:18 +0300

    Update kot.py

--- kot.py
+++ kot.py
@@ -20,7 +20,6 @@ tokenizer = Tokenizer()
 tokenizer.fit_on_texts(questions + answers)
 total_words = len(tokenizer.word_index) + 1
 
-# Добавление <start> и <end> в словарь токенизатора
 tokenizer.word_index['<start>'] = total_words
 tokenizer.word_index['<end>'] = total_words + 1
 total_words += 2
@@ -47,89 +46,8 @@ for i, seq in enumerate(tokenized_answers):
 embedding_dim = 64
 hidden_units = 128
 
-encoder_inputs = tf.keras.layers.Input(shape=(max_sequence_len,))
-encoder_embedding = tf.keras.layers.Embedding(total_words, embedding_dim, mask_zero=True)(encoder_inputs)
-encoder_lstm = tf.keras.layers.LSTM(hidden_units, return_sequences=True, return_state=True)
-encoder_outputs, state_h, state_c = encoder_lstm(encoder_embedding)
-encoder_states = [state_h, state_c]
-
-decoder_inputs = tf.keras.layers.Input(shape=(max_sequence_len,))
-decoder_embedding = tf.keras.layers.Embedding(total_words, embedding_dim, mask_zero=True)(decoder_inputs)
-decoder_lstm = tf.keras.layers.LSTM(hidden_units, return_sequences=True, return_state=True)
-decoder_outputs, _, _ = decoder_lstm(decoder_embedding, initial_state=encoder_states)
-
-attention = tf.keras.layers.Attention()
-attention_output = attention([decoder_outputs, encoder_outputs])
-
-decoder_concat_input = tf.keras.layers.Concatenate(axis=-1)([decoder_outputs, attention_output])
-decoder_dense = tf.keras.layers.Dense(total_words, activation='softmax')
-output = decoder_dense(decoder_concat_input)
-
-model = tf.keras.models.Model([encoder_inputs, decoder_inputs], output)
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-# Обучение модели seq2seq
-model.fit(
-    [encoder_input_data, decoder_input_data],
-    decoder_output_data.reshape(*decoder_output_data.shape, 1),
-    batch_size=64,
-    epochs=500
-)
-
-# Создание модели энкодера
-encoder_model = tf.keras.models.Model(encoder_inputs, [encoder_outputs, state_h, state_c])
-
-# Создание модели декодера
-decoder_state_input_h = tf.keras.layers.Input(shape=(hidden_units,))
-decoder_state_input_c = tf.keras.layers.Input(shape=(hidden_units,))
-decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
-
-decoder_outputs, state_h, state_c = decoder_lstm(decoder_embedding, initial_state=decoder_states_inputs)
-decoder_states = [state_h, state_c]
-decoder_model = tf.keras.models.Model(
-    [decoder_inputs] + decoder_states_inputs,
-    [decoder_outputs] + decoder_states
-)
-
-
-async def generate_response(input_text):
-    input_seq = tokenizer.texts_to_sequences([input_text])
-    input_seq = pad_sequences(input_seq, maxlen=max_sequence_len, padding='post')
-
-
-    # Получение начальных состояний энкодера
-    _, h, c = encoder_model.predict(input_seq)
-
-    # Инициализация декодера начальными состояниями
-    target_seq = np.zeros((1, 1))
-    target_seq[0, 0] = tokenizer.word_index['<start>']
-
-    stop_condition = False
-    decoded_sentence = []
-
-    while not stop_condition:
-        # Предсказание следующего слова из декодера
-        output_tokens, h, c = decoder_model.predict([target_seq, h, c])
-
-        # Получение индекса слова с наибольшей вероятностью
-        sampled_token_index = np.argmax(output_tokens[0, -1, :])
-
-        # Получение слова по индексу из словаря
-        sampled_word = tokenizer.index_word.get(sampled_token_index, '')
-
-        # Обработка неизвестных слов и остановка
-        if sampled_word != '<end>' and sampled_word != '':
-            decoded_sentence.append(sampled_word)
-
-        if sampled_word == '<end>' or len(decoded_sentence) >= max_sequence_len:
-            stop_condition = True
-
-        # Обновление последовательности для декодера
-        target_seq = np.zeros((1, 1))
-        target_seq[0, 0] = sampled_token_index
-
-    return ' '.join(decoded_sentence)
-
+# Остальная часть кода остаётся без изменений
+# ...
 
 # Обновление токенизатора после изменения словаря
 tokenizer = Tokenizer()
@@ -145,7 +63,6 @@ total_words += 2
 
 # Инициализация бота и обработка сообщений
 # ...
-
 
 # Инициализация бота и обработка сообщений
 bot = aiogram.Bot(token="6439522576:AAGBJahBMqhUDlaikziF3Dqm3lEdE4a6mL0")
